ada1cbc09ce3d35eb41817fcf158230509049461
Scott McAllister <smcallister@pagerduty.com>
2022-01-18 22:27:57 -0800

    go fmt

--- labels_test.go
+++ labels_test.go
@@ -23,38 +23,37 @@ func TestDiscogs_LabelsGet(t *testing.T) {
 		t.Fatal(err)
 	}
 	want := &Label{
-			ID: 99512,
-			Name: "Temporary Residence Limited",
-			ResourceURL: "https://api.discogs.com/labels/9952",
-			URI: "https://www.discogs.com/label/9952-Temporary-Residence-Limited",
-			ReleasesURL: "https://api.discogs.com/labels/9952/releases",
-			Images: []*Image{
-				{
-				Type: "primary",
-				URI: "https://i.discogs.com/3WhxhnP1uMwu9Lo7iX-UkuKgFsMBVJVyiS_YIZgcQ_A/rs:fit/g:sm/q:90/h:636/w:600/czM6Ly9kaXNjb2dz/LWltYWdlcy9MLTk5/NTItMTYwMDg0Njg1/OC0zNTM2LmpwZWc.jpeg",
+		ID:          99512,
+		Name:        "Temporary Residence Limited",
+		ResourceURL: "https://api.discogs.com/labels/9952",
+		URI:         "https://www.discogs.com/label/9952-Temporary-Residence-Limited",
+		ReleasesURL: "https://api.discogs.com/labels/9952/releases",
+		Images: []*Image{
+			{
+				Type:        "primary",
+				URI:         "https://i.discogs.com/3WhxhnP1uMwu9Lo7iX-UkuKgFsMBVJVyiS_YIZgcQ_A/rs:fit/g:sm/q:90/h:636/w:600/czM6Ly9kaXNjb2dz/LWltYWdlcy9MLTk5/NTItMTYwMDg0Njg1/OC0zNTM2LmpwZWc.jpeg",
 				ResourceURL: "https://i.discogs.com/3WhxhnP1uMwu9Lo7iX-UkuKgFsMBVJVyiS_YIZgcQ_A/rs:fit/g:sm/q:90/h:636/w:600/czM6Ly9kaXNjb2dz/LWltYWdlcy9MLTk5/NTItMTYwMDg0Njg1/OC0zNTM2LmpwZWc.jpeg",
-				URI150: "https://i.discogs.com/NLzhVuBlqcurOzYMEr89GPz0PeuevwvHkodFzgyFDi0/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWltYWdlcy9MLTk5/NTItMTYwMDg0Njg1/OC0zNTM2LmpwZWc.jpeg",
-				Width: 600,
-				Height: 636,
-				},
+				URI150:      "https://i.discogs.com/NLzhVuBlqcurOzYMEr89GPz0PeuevwvHkodFzgyFDi0/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWltYWdlcy9MLTk5/NTItMTYwMDg0Njg1/OC0zNTM2LmpwZWc.jpeg",
+				Width:       600,
+				Height:      636,
 			},
-			ContactInfo: "Temporary Residence Limited\r\nPO Box 60097\r\nBrooklyn\r\nNY 11206\r\nUSA\r\n\r\ntrl@temporaryresidence.com\r\ninfo@temporaryresidence.com\r\n\r\n(1997)\r\nThe Temporary Residence Limited\r\nPost Office Box 22910\r\nBaltimore, Maryland 21203-4910\r\nUSA",
-			Profile: "Independent label based in Brooklyn, New York. Established 1996.\r\n[b]For the company and holder of copyrights, use [l203831][/b]",
-			DataQuality: "Correct",
-			URLs: []string{
-				"http://temporaryresidence.com",
-				"http://www.facebook.com/temporaryresidence",
+		},
+		ContactInfo: "Temporary Residence Limited\r\nPO Box 60097\r\nBrooklyn\r\nNY 11206\r\nUSA\r\n\r\ntrl@temporaryresidence.com\r\ninfo@temporaryresidence.com\r\n\r\n(1997)\r\nThe Temporary Residence Limited\r\nPost Office Box 22910\r\nBaltimore, Maryland 21203-4910\r\nUSA",
+		Profile:     "Independent label based in Brooklyn, New York. Established 1996.\r\n[b]For the company and holder of copyrights, use [l203831][/b]",
+		DataQuality: "Correct",
+		URLs: []string{
+			"http://temporaryresidence.com",
+			"http://www.facebook.com/temporaryresidence",
+		},
+		SubLabels: []*SubLabel{
+			{
+				ID:          648273,
+				Name:        "Sounds Of The Geographically Challenged",
+				ResourceURL: "https://api.discogs.com/labels/648273",
 			},
-			SubLabels: []*SubLabel{
-				{
-					ID : 648273,
-					Name: "Sounds Of The Geographically Challenged",
-					ResourceURL: "https://api.discogs.com/labels/648273",
-				},
-			},
-		}
+		},
+	}
 
 	testEqual(t, want, rat)
 
 }
-
